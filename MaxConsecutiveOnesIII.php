4706f06ac117479eb7d6324eb0e023102bd896d2
Inaba Kenji <k.inaba040332@gmail.com>
2023-12-30 15:43:03 +0900

    Initial commit

---
+++ MaxConsecutiveOnesIII.php
@@ -0,0 +1,14 @@
+<?php
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @param Integer $k
+   * @return Integer
+   */
+  function longestOnes($nums, $k) {
+
+  }
+}
