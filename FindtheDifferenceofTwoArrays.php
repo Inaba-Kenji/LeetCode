ee9e51bc9be7d373f76b3c24fdb0f2f188ce3ab1
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-01 21:32:44 +0900

    Initial commit

---
+++ FindtheDifferenceofTwoArrays.php
@@ -0,0 +1,18 @@
+<?php
+// Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+// Note that the integers in the lists may be returned in any order.
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums1
+   * @param Integer[] $nums2
+   * @return Integer[][]
+   */
+  function findDifference($nums1, $nums2) {
+
+  }
+}
