694bc110b7692c09e0528eed7dde8f1b4ebdfd90
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-02 01:31:22 +0900

    Initial commit

---
+++ UniqueNumberofOccurrences.php
@@ -0,0 +1,13 @@
+<?php
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+class Solution {
+
+  /**
+   * @param Integer[] $arr
+   * @return Boolean
+   */
+  function uniqueOccurrences($arr) {
+
+  }
+}
