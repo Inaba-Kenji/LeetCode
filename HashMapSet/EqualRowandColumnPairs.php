4d476bfe5b86176bbea292b9134f9053359f5f4c
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-03 17:56:43 +0900

    2352 Initial commit

---
+++ HashMapSet/EqualRowandColumnPairs.php
@@ -0,0 +1,15 @@
+<?php
+// Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+
+// A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+
+class Solution {
+
+  /**
+   * @param int[][] $grid
+   * @return int
+   */
+  function equalPairs($grid) {
+
+  }
+}
