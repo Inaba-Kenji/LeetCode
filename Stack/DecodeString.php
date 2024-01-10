427e00f3eece8c85b6ae0ea32b6e01190ddf8d46
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-11 08:56:41 +0900

    [finish] Stack/DecodeString.php

--- Stack/DecodeString.php
+++ Stack/DecodeString.php
@@ -14,6 +14,39 @@ class Solution {
    * @return String
    */
   function decodeString($s) {
+    $i = $num = 0;
+    // "abc3[cd]xyz"のパターンに対応するため $stack = []ではない
+    $stack = [""];
 
+    while ($i < strlen($s)) {
+      if (is_numeric($s[$i])) {
+        // 数字が２桁の場合を想定して
+        $num = $num * 10 + intval($s[$i]);
+      } elseif ($s[$i] == "[") {
+        // "["の次は文字が来るので数値をスタックに追加する
+        // 文字が来た場合に連結しやすくするため
+        array_push($stack, $num);
+        $num = 0;
+        array_push($stack, "");
+      } elseif ($s[$i] == "]") {
+        // 文字と数字で繰り返しの処理を実行
+        // str2に連結しているのは括弧の中に括弧があるパターンに対応するため
+        $str1 = array_pop($stack);
+        $rep  = array_pop($stack);
+        $str2  = array_pop($stack);
+        array_push($stack, $str2 . str_repeat($str1, intval($rep)));
+      } else {
+        // 文字列が何文字でも対応できるように連結する
+        $stack[count($stack) - 1] .= $s[$i];
+      }
+      $i++;
+    }
+    return implode("", $stack);
   }
 }
+
+// 使用例:
+$solution = new Solution;
+$input = "3[a2[c]]";
+$result = $solution->decodeString($input);
+echo $result;  // 出力: "accaccacc"
