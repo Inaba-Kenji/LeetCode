7136b283493bb47d21f83c622e2516a8f3a65a56
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-13 16:47:19 +0900

    2095 Initial commit

---
+++ LinkedList/DeletetheMiddleNodeofaLinkedList.php
@@ -0,0 +1,28 @@
+<?php
+// You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+// The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+// For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @return ListNode
+   */
+  function deleteMiddle($head) {
+
+  }
+}
