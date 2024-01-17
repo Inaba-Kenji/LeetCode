d57ff9cfa8a25447f86b78110694902ebc19a017
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-17 09:40:30 +0900

    206 Initial commit

---
+++ LinkedList/ReverseLinkedList.php
@@ -0,0 +1,24 @@
+<?php
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @return ListNode
+   */
+  function reverseList($head) {
+
+  }
+}
