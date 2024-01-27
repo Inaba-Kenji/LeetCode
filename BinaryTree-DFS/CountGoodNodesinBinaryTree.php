efa5709a5f58b9e9c8899282e8c25d9d9f0ca7cb
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-27 12:48:57 +0900

    1448 Initial commit

---
+++ BinaryTree-DFS/CountGoodNodesinBinaryTree.php
@@ -0,0 +1,31 @@
+<?php
+
+// Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+// Return the number of good nodes in the binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param TreeNode $root
+   * @return Integer
+   */
+  function goodNodes($root) {
+
+  }
+}
+
+
