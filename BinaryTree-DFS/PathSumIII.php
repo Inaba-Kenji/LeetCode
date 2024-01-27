4959f939a7735c867ccb92cf184f5ad1c2c1e31c
Inaba Kenji <k.inaba040332@gmail.com>
2024-01-27 14:48:16 +0900

    437 Initial commit

---
+++ BinaryTree-DFS/PathSumIII.php
@@ -0,0 +1,29 @@
+<?php
+// Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+// The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param TreeNode $root
+   * @param Integer $targetSum
+   * @return Integer
+   */
+  function pathSum($root, $targetSum) {
+
+  }
+}
