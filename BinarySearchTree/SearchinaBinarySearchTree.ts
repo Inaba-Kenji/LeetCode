e77b1e376ae42ab875f3c42680bf159fa6ca1930
Inaba Kenji <k.inaba040332@gmail.com>
2024-04-22 15:46:21 +0900

    [finish] BinarySearchTree/SearchinaBinarySearchTree.ts

---
+++ BinarySearchTree/SearchinaBinarySearchTree.ts
@@ -0,0 +1,25 @@
+// You are given the root of a binary search tree (BST) and an integer val.
+
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (root == null) return null;
+  if (root.val == val) {
+    return root;
+  } else {
+    return val < root.val
+      ? searchBST(root.left, val)
+      : searchBST(root.right, val);
+  }
+}
